32d7a12b5e7f5b72b2b6dc96e960cdb0fc26f24e
matzesoft <matzesoft@gmail.com>
2024-11-11 23:20:46 +0100

    Add VibrationDetection + PollingLoop

--- DynamicLedMatrix/DynamicLedMatrix.ino
+++ DynamicLedMatrix/DynamicLedMatrix.ino
@@ -1,19 +1,38 @@
 #include <Arduino.h>
 
 #include "TempReader.h"
+#include "VibrationDetection.h"
+
+unsigned long lastPollingMillis = 0;
+const unsigned long pollingInterval = 500;
 
 TempReader tempReader = TempReader();
+VibrationDetection vibrationDetection = VibrationDetection();
 
 void setup(void) {
   tempReader.begin();
+  vibrationDetection.begin();
 }
 
 void loop() {
+  // Polling Loop
+  unsigned long currentMillis = millis();
+  if (currentMillis - lastPollingMillis >= pollingInterval) {
+    pollingLoop();
+  }
+
+  // Regular Loop
+}
+
+// Loop which only runs in the specified `pollingInterval`.
+void pollingLoop() {
   tempReader.update();
+  vibrationDetection.update();
 
   Serial.print("Sourrounding Temperature = ");
   Serial.print(tempReader.temp);
   Serial.println(" °C");
 
-  delay(500);
+  Serial.print("Active Vibrations = ");
+  Serial.println(vibrationDetection.activeVib);
 }
