4108d1c64854e2f258a692851b92a162b70e25c2
matzesoft <matzesoft@gmail.com>
2024-11-14 16:52:15 +0100

    Implement LedMatrix based on emotions

--- DynamicLedMatrix/LedMatrix.cpp
+++ DynamicLedMatrix/LedMatrix.cpp
@@ -1,3 +1,4 @@
+#include <sys/_types.h>
 #include "LedMatrix.h"
 
 /* ------------- PROTOMATTER CONTEXT ------------- */
@@ -36,6 +37,20 @@ void LedMatrix::begin() {
   https://learn.adafruit.com/adafruit-gfx-graphics-library/graphics-primitives#extended-characters-cp437-and-a-lurking-bug-3100368
   */
   matrix.cp437(true);
+
+  // Calc general matrix constants
+  centerX = matrix.width() / 2;
+
+  // Calc constants for NEUTRAL emtotion
+  matrix.getTextBounds("20", 0, 0, nullptr, nullptr, nullptr, &neutral_statusBarHeight);  // Use 20°C as height reference for temperatures
+
+  // Calc constants for Schkub
+  schkub_centerX = centerX;
+  schkub_centerY = ((matrix.height() - neutral_statusBarHeight) / 2) + neutral_statusBarHeight;
+  schkub_leftEyeTopX = schkub_centerX - SCHKUB_EYE_WIDTH - 1;
+  schkub_rightEyeTopX = schkub_centerX + 2;
+  schkub_leftEyeTopY = schkub_centerY - SCHKUB_EYE_HEIGHT + 1;
+  schkub_rightEyeTopY = schkub_centerY - SCHKUB_EYE_HEIGHT + 1;
 }
 
 void LedMatrix::update(int temperature, Emotion emotion) {
@@ -43,13 +58,13 @@ void LedMatrix::update(int temperature, Emotion emotion) {
 
   switch (emotion) {
     case Emotion::NEUTRAL:
-      drawNeutralEmotion(temperature);
+      neutral_drawEmotion(temperature);
       break;
     case Emotion::TOO_LOUD:
-      drawTooLoudEmotion();
+      tooLoud_drawEmotion();
       break;
     case Emotion::SHAKY:
-      drawShakyEmotion();
+      shaky_drawEmotion();
       break;
   }
 
@@ -58,29 +73,98 @@ void LedMatrix::update(int temperature, Emotion emotion) {
 
 /* --------------- PRIVATE FUNCTIONS --------------- */
 
-void LedMatrix::drawNeutralEmotion(int temperature) {
-  matrix.setCursor(0, 0);
+void LedMatrix::schkub_drawBackground() {
+  matrix.fillCircle(schkub_centerX, schkub_centerY, SCHKUB_RADIUS, BLUE);
+}
+
+void LedMatrix::schkub_drawEyes() {
+  unsigned long currentMillis = millis();
+  unsigned long millisSinceReactionStarted = currentMillis - schkub_reactionStartedMillis;
+
+  // Start new reaction
+  if (millisSinceReactionStarted >= schkub_reactionInterval) {
+    randomSeed(currentMillis);
+
+    schkub_reactionStartedMillis = currentMillis;
+    schkub_reactionInterval = random(SCHKUB_REACTION_MIN_INTERVAL, SCHKUB_REACTION_MAX_INTERVAL);
+
+    // Change reaction type
+    schkub_reaction = static_cast<SchkubReactions>(random(0, SCHKUB_REACTIONS_COUNT));
+
+    // End current reaction
+  } else if (millisSinceReactionStarted >= SCHKUB_REACTION_DURATION) {
+    schkub_reaction = SchkubReactions::NONE;
+  }
+
+  // Update current reaction
+  switch (schkub_reaction) {
+    case SchkubReactions::BLINK_EYES:
+      matrix.fillRoundRect(schkub_leftEyeTopX, schkub_leftEyeTopY, SCHKUB_EYE_WIDTH, SCHKUB_EYE_HEIGHT, 1, WHITE);
+      matrix.fillRoundRect(schkub_rightEyeTopX, schkub_rightEyeTopY, SCHKUB_EYE_WIDTH, SCHKUB_EYE_HEIGHT, 1, WHITE);
+      break;
+
+    case SchkubReactions::LOOK_LEFT:
+      matrix.fillRoundRect(schkub_leftEyeTopX - 1, schkub_leftEyeTopY, SCHKUB_EYE_WIDTH, SCHKUB_EYE_HEIGHT, 1, BLACK);
+      matrix.fillRoundRect(schkub_rightEyeTopX - 1, schkub_rightEyeTopY, SCHKUB_EYE_WIDTH, SCHKUB_EYE_HEIGHT, 1, BLACK);
+      matrix.drawRoundRect(schkub_leftEyeTopX - 1, schkub_leftEyeTopY, SCHKUB_EYE_WIDTH, SCHKUB_EYE_HEIGHT, 1, WHITE);
+      matrix.drawRoundRect(schkub_rightEyeTopX - 1, schkub_rightEyeTopY, SCHKUB_EYE_WIDTH, SCHKUB_EYE_HEIGHT, 1, WHITE);
+      break;
+
+    case SchkubReactions::LOOK_RIGHT:
+      matrix.fillRoundRect(schkub_leftEyeTopX + 1, schkub_leftEyeTopY, SCHKUB_EYE_WIDTH, SCHKUB_EYE_HEIGHT, 1, BLACK);
+      matrix.fillRoundRect(schkub_rightEyeTopX + 1, schkub_rightEyeTopY, SCHKUB_EYE_WIDTH, SCHKUB_EYE_HEIGHT, 1, BLACK);
+      matrix.drawRoundRect(schkub_leftEyeTopX + 1, schkub_leftEyeTopY, SCHKUB_EYE_WIDTH, SCHKUB_EYE_HEIGHT, 1, WHITE);
+      matrix.drawRoundRect(schkub_rightEyeTopX + 1, schkub_rightEyeTopY, SCHKUB_EYE_WIDTH, SCHKUB_EYE_HEIGHT, 1, WHITE);
+      break;
+
+    default:
+      matrix.fillRoundRect(schkub_leftEyeTopX, schkub_leftEyeTopY, SCHKUB_EYE_WIDTH, SCHKUB_EYE_HEIGHT, 1, BLACK);
+      matrix.fillRoundRect(schkub_rightEyeTopX, schkub_rightEyeTopY, SCHKUB_EYE_WIDTH, SCHKUB_EYE_HEIGHT, 1, BLACK);
+      matrix.drawRoundRect(schkub_leftEyeTopX, schkub_leftEyeTopY, SCHKUB_EYE_WIDTH, SCHKUB_EYE_HEIGHT, 1, WHITE);
+      matrix.drawRoundRect(schkub_rightEyeTopX, schkub_rightEyeTopY, SCHKUB_EYE_WIDTH, SCHKUB_EYE_HEIGHT, 1, WHITE);
+      break;
+  }
+}
+
+
+void LedMatrix::neutral_drawEmotion(int temperature) {
+  neutral_drawTemperature(temperature);
+
+  schkub_drawBackground();
+  schkub_drawEyes();
+}
+
+void LedMatrix::neutral_drawTemperature(int temperature) {
+  matrix.setCursor(8, 0);
+  matrix.setTextColor(WHITE);
   matrix.print(temperature);
   matrix.write(0xF8);  // Print the degrees symbol
   matrix.println();
-
-  int16_t x = matrix.width() / 2;
-  matrix.fillCircle(x, 20, 8, 0x001F);
-
-  matrix.drawRoundRect(x - 4, 16, 3, 5, 1, 0xFFFF);
-  matrix.drawRoundRect(x + 2, 16, 3, 5, 1, 0xFFFF);
 }
 
-void LedMatrix::drawTooLoudEmotion() {
-  matrix.setCursor(0, 0);
-  matrix.println("TOO LOUD");
+void LedMatrix::tooLoud_drawEmotion() {
+  matrix.setCursor(4, 0);
+  matrix.setTextColor(YELLOW);
+  matrix.println("LOUD");
 
-  // TODO: Implement
+  matrix.fillCircle(schkub_centerX, schkub_centerY + 7, SCHKUB_RADIUS, BLUE);
+  matrix.fillRoundRect(schkub_leftEyeTopX, schkub_leftEyeTopY + 6, SCHKUB_EYE_WIDTH, SCHKUB_EYE_HEIGHT + 1, 1, WHITE);
+  matrix.fillRoundRect(schkub_rightEyeTopX, schkub_rightEyeTopY + 6, SCHKUB_EYE_WIDTH, SCHKUB_EYE_HEIGHT + 1, 1, WHITE);
+
+  matrix.setCursor(schkub_centerX - 2, schkub_centerY - SCHKUB_RADIUS - 1);
+  matrix.setTextColor(WHITE);
+  matrix.print("!");
 }
 
-void LedMatrix::drawShakyEmotion() {
-  matrix.setCursor(0, 0);
+void LedMatrix::shaky_drawEmotion() {
+  matrix.setCursor(1, 0);
+  matrix.setTextColor(YELLOW);
   matrix.println("SHAKY");
 
-  // TODO: Implement
+  matrix.fillCircle(schkub_centerX, schkub_centerY + 2, SCHKUB_RADIUS, BLUE);
+  matrix.fillRoundRect(schkub_leftEyeTopX, schkub_leftEyeTopY + 4, SCHKUB_EYE_WIDTH, SCHKUB_EYE_HEIGHT - 1, 1, WHITE);
+  matrix.fillRoundRect(schkub_rightEyeTopX, schkub_rightEyeTopY + 4, SCHKUB_EYE_WIDTH, SCHKUB_EYE_HEIGHT - 1, 1, WHITE);
+
+  matrix.drawLine(schkub_centerX - 3, schkub_centerY - SCHKUB_RADIUS + 1 - 3, schkub_centerX + 3, schkub_centerY - SCHKUB_RADIUS + 1 + 3, RED);
+  matrix.drawLine(schkub_centerX - 3, schkub_centerY - SCHKUB_RADIUS + 1 + 3, schkub_centerX + 3, schkub_centerY - SCHKUB_RADIUS + 1 - 3, RED);
 }
--- DynamicLedMatrix/LedMatrix.h
+++ DynamicLedMatrix/LedMatrix.h
@@ -1,3 +1,4 @@
+#include <sys/_stdint.h>
 #ifndef LedMatrix_h
 #define LedMatrix_h
 
@@ -12,9 +13,54 @@ public:
   void update(int temperature, Emotion emotion);
 
 private:
-  void drawNeutralEmotion(int temperature);
-  void drawTooLoudEmotion();
-  void drawShakyEmotion();
+  // Color definitions
+  const uint16_t WHITE = 0xFFFF;
+  const uint16_t BLACK = 0x0000;
+  const uint16_t BLUE = 0x001F;
+  const uint16_t RED = 0xF800;
+  const uint16_t YELLOW = 0xFFE0;
+  const uint16_t GREEN = 0x07E0;
+
+  // General matrix variables
+  uint16_t centerX;
+
+  // Schkub matrix variables
+  enum SchkubReactions {
+    NONE,
+    BLINK_EYES,
+    LOOK_LEFT,
+    LOOK_RIGHT
+  };
+  const uint8_t SCHKUB_REACTIONS_COUNT = 4;
+
+  const uint16_t SCHKUB_RADIUS = 8;
+  const uint16_t SCHKUB_EYE_HEIGHT = 5;
+  const uint16_t SCHKUB_EYE_WIDTH = 3;
+  const unsigned long SCHKUB_REACTION_DURATION = 1800;
+  const unsigned long SCHKUB_REACTION_MIN_INTERVAL = 4000;
+  const unsigned long SCHKUB_REACTION_MAX_INTERVAL = 8000;
+  SchkubReactions schkub_reaction = SchkubReactions::NONE;
+  uint16_t schkub_centerX;
+  uint16_t schkub_centerY;
+  uint16_t schkub_leftEyeTopX;
+  uint16_t schkub_leftEyeTopY;
+  uint16_t schkub_rightEyeTopX;
+  uint16_t schkub_rightEyeTopY;
+  unsigned long schkub_reactionStartedMillis = 0;
+  unsigned long schkub_reactionInterval = 0;
+  void schkub_drawBackground();
+  void schkub_drawEyes();
+
+  // NEUTRAL matrix variables
+  uint16_t neutral_statusBarHeight;
+  void neutral_drawEmotion(int temperature);
+  void neutral_drawTemperature(int temperature);
+
+  // TOO_LOUD matrix variables
+  void tooLoud_drawEmotion();
+
+  // SHAKY matrix variables
+  void shaky_drawEmotion();
 };
 
 #endif /* LedMatrix */
